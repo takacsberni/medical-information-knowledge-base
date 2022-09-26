bdcf9f0de260e41f0225de305f53e3c2e6c55b05
takacsberni <takacsberni@gmail.com>
2022-09-26 17:57:17 +0200

    feat: add some lombok annotations for less code

--- src/main/java/com/ancestralize/knowledgebase/model/Disease.java
+++ src/main/java/com/ancestralize/knowledgebase/model/Disease.java
@@ -1,7 +1,15 @@
 package com.ancestralize.knowledgebase.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.util.Set;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Disease {
     private String name;
     private String description;
