1da641146cd57d71a263b8bf0d99e3fc91120d56
takacsberni <takacsberni@gmail.com>
2022-09-26 17:58:17 +0200

    feat: create RiskFactors class

---
+++ src/main/java/com/ancestralize/knowledgebase/model/RiskFactors.java
@@ -0,0 +1,15 @@
+package com.ancestralize.knowledgebase.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class RiskFactors {
+    private String name;
+    private String description;
+}
