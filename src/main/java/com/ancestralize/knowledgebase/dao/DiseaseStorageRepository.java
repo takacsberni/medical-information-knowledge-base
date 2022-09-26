b3f76451e66498dc000047dd170cf3b69fe65b96
takacsberni <takacsberni@gmail.com>
2022-09-26 19:53:37 +0200

    feat: create an interface in dao layer to extend JpaRepository - this will be the base of connection with Spring Data Jpa

---
+++ src/main/java/com/ancestralize/knowledgebase/dao/DiseaseStorageRepository.java
@@ -0,0 +1,9 @@
+package com.ancestralize.knowledgebase.dao;
+
+import com.ancestralize.knowledgebase.model.Disease;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DiseaseStorageRepository extends JpaRepository<Disease, UUID> {
+}
