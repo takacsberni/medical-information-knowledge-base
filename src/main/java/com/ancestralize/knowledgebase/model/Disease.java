2545852340436008ec44ae039dc9b361f93ca7bd
takacsberni <takacsberni@gmail.com>
2022-09-26 19:51:18 +0200

    feat: mark Disease as an entity (so it is now a table in the database)

--- src/main/java/com/ancestralize/knowledgebase/model/Disease.java
+++ src/main/java/com/ancestralize/knowledgebase/model/Disease.java
@@ -4,15 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.Set;
+import java.util.UUID;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Disease {
+    @Id
+    @GeneratedValue
+    private UUID id;
     private String name;
     private String description;
-    private Set<RiskFactors> riskFactors;
-    private Set<Symptoms> symptoms;
+
+//    private Set<RiskFactors> riskFactors;
+//
+//    private Set<Symptoms> symptoms;
 }
