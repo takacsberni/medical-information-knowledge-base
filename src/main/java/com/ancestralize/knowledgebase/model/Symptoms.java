89642b68a1c9dc9f6f2167dcefab788355539704
takacsberni <takacsberni@gmail.com>
2022-09-26 17:58:44 +0200

    feat: create Symptoms class

---
+++ src/main/java/com/ancestralize/knowledgebase/model/Symptoms.java
@@ -0,0 +1,15 @@
+package com.ancestralize.knowledgebase.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Symptoms {
+    private String name;
+    private String description;
+}
