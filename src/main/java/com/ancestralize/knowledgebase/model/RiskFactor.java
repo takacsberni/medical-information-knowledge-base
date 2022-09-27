89dd61a778dd4f9efa147e6f727f7699d58c1da3
takacsberni <takacsberni@gmail.com>
2022-09-27 12:03:16 +0200

    feat: fill RiskFactor and Symptom with some data

--- src/main/java/com/ancestralize/knowledgebase/model/RiskFactor.java
+++ src/main/java/com/ancestralize/knowledgebase/model/RiskFactor.java
@@ -3,7 +3,15 @@ package com.ancestralize.knowledgebase.model;
 public enum RiskFactor {
     LACK_OF_EXERCISE("Physical inactivity is one important cause of most chronic diseases."),
     LOW_PROTEIN_INTAKE("Protein deficiency is when your intake is unable to meet your body’s requirements."),
-    OLD_AGE("Old age refers to ages nearing or surpassing the life expectancy");
+    OLD_AGE("Old age refers to ages nearing or surpassing the life expectancy"),
+    PREVIOUS_INJURIES_TO_A_JOINT(""),
+    OVERUSE_A_MUSCLE(""),
+    REPEATEDLY_USE_A_MUSCLE(""),
+    OVERWEIGHT(""),
+    GENETIC_MARKER("When you have the genetic marker that makes you susceptible to something"),
+    TICK_BITE("");
+
+
 
 
     private String description;
--- src/main/java/com/ancestralize/knowledgebase/model/Symptom.java
+++ src/main/java/com/ancestralize/knowledgebase/model/Symptom.java
@@ -3,7 +3,17 @@ package com.ancestralize.knowledgebase.model;
 public enum Symptom {
     BONE_FRACTURES("A medical condition in which there is a partial or complete break in the continuity of a bone."),
     HEIGHT_LOSS("Height loss is related to aging changes in bone, muscles, and joints"),
-    POSTURE_CHANGE("Height is lost in all men and women as they age. When poor posture leads to disc compression, the vertebrae in our back absorbs more force than it should. This can lead to chronic pain.");
+    POSTURE_CHANGE("Height is lost in all men and women as they age. When poor posture leads to disc compression, the vertebrae in our back absorbs more force than it should. This can lead to chronic pain."),
+    JOINT_PAIN("Joint discomfort is common and usually felt in the hands, feet, hips, knees, or spine."),
+    FEELING_VERY_THIRSTY(""),
+    FEELING_VERY_TIRED(""),
+    URINATING_MORE_FREQUENTLY_THAN_USUAL(""),
+    WEIGHT_LOSS(""),
+    CIRCULAR_RASH_AROUND_THE_BITE(""),
+    MUSCLE_PAIN(""),
+    HEADACHES(""),
+    FEVER("");
+
 
     private String description;
 
