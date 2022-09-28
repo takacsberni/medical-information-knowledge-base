c96371355aa30927e9c32a3a060247173b60b9df
takacsberni <takacsberni@gmail.com>
2022-09-28 13:07:39 +0200

    feat: create a basic footer component

---
+++ src/main/ui/src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="flex-container-footer">
+                <div className="footer-col-1">
+                    <h5 className="text-md-left"> test </h5>
+                </div>
+                <div className="footer-col-2">
+                    <h5 className="middle"> test</h5>
+                </div>
+                <div className="footer-col-3">
+                    <h5 className="middle"> test </h5>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
