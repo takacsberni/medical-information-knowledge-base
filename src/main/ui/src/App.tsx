d198943849bc773d4f11d22e56a589905605cd2f
takacsberni <takacsberni@gmail.com>
2022-09-28 13:08:48 +0200

    feat: App renders Footer and an Outlet too

--- src/main/ui/src/App.tsx
+++ src/main/ui/src/App.tsx
@@ -2,13 +2,15 @@ import React, {FC} from 'react';
 import './App.css';
 import {Outlet} from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 
 const App: FC = () => {
   return (
     <div className="App">
         <div className="flex-container-home">
             <div className="navbar"> <NavigationBar /> </div>
-
+            <div className="inside"> <Outlet /></div>
+            <div className="footer"> <Footer /></div>
         </div>
     </div>
   );
