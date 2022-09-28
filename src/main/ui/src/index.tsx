4bbdbb6369b1ec0916e514fc35fffba597d21b61
takacsberni <takacsberni@gmail.com>
2022-09-28 18:42:01 +0200

    feat: define the route "disease/:filter" (which is a link in the navigation bar) gives back AllDisease

--- src/main/ui/src/index.tsx
+++ src/main/ui/src/index.tsx
@@ -4,8 +4,7 @@ import App from './App';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./components/Home";
-
-
+import AllDiseases from "./routes/AllDiseases";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,6 +14,7 @@ root.render(
       <Routes>
           <Route path="/" element={<App />}>
               <Route path="" element={<Home />} />
+              <Route path="diseases/:filter" element={<AllDiseases />} />
           </Route>
       </Routes>
   </BrowserRouter>
