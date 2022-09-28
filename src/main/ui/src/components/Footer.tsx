07541a2ec48577569a9799c29e423554ca31dd3d
takacsberni <takacsberni@gmail.com>
2022-09-28 15:23:39 +0200

    feat: add a Footer component

--- src/main/ui/src/components/Footer.tsx
+++ src/main/ui/src/components/Footer.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import '../static/css/footer.css';
 
 const Footer: React.FC = () => {
     return (
         <footer>
             <div className="flex-container-footer">
                 <div className="footer-col-1">
-                    <h5 className="text-md-left"> test </h5>
+                    <h5 className="text-md-left"> <a href="mailto: dev@ancestralize.com"> Need help? </a> </h5>
                 </div>
                 <div className="footer-col-2">
-                    <h5 className="middle"> test</h5>
+                    <h5 className="middle"> <a href="https://www.ancestralize.com/"> Ancestralize home </a> </h5>
                 </div>
                 <div className="footer-col-3">
-                    <h5 className="middle"> test </h5>
+                    <h5 className="middle"> 2022 </h5>
                 </div>
             </div>
         </footer>
