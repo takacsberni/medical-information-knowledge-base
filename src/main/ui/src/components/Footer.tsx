5caa8034718e0675448542fb9b2f1fb596cd5766
takacsberni <takacsberni@gmail.com>
2022-09-29 17:07:07 +0200

    feat: add some toggle tips for the footer's elements

--- src/main/ui/src/components/Footer.tsx
+++ src/main/ui/src/components/Footer.tsx
@@ -9,13 +9,14 @@ const Footer = ({changeTheme}:Props) => {
     return (
         <footer>
             <div className="flex-container-footer">
-                <div className="footer-col-1">
+                <div className="footer-col-1" data-toggle="tooltip" title="Email to developers">
                     <h5 className="text-md-left"> <a href="mailto: dev@ancestralize.com"> Need help? </a> </h5>
                 </div>
-                <div className="footer-col-2">
+                <div className="footer-col-2" data-toggle="tooltip" title="Go to center">
                     <h5 className="middle"> <a href="https://www.ancestralize.com/"> Ancestralize home </a> </h5>
                 </div>
-                <div className="footer-col-3" onClick={() => {changeTheme()}}>
+                <div className="footer-col-3" onClick={() => {changeTheme()}}
+                     data-toggle="tooltip" title="Change between normal and dark mode">
                     <h5 className="middle"> Color </h5>
                 </div>
             </div>
